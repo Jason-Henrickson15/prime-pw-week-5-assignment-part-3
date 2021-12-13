30c7bf275853a5c3556688010563e99e63dfdbd9
Jason-Henrickson15 <90575304+Jason-Henrickson15@users.noreply.github.com>
2021-12-12 21:05:03 -0600

    Update 3-music-collection.js

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,26 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(songTitle,songArtist,songYearPublished){
+  const songInfo = {
+    song: songTitle,
+    artist: songArtist,
+    year: songYearPublished
+  }
+  collection.push(songInfo);
+}
+
+addToCollection("songTitle","songArtist","someYear");
+console.log(collection[0]);
+
+
+function showCollection(arrayItem){
+  object = collection[arrayItem];
+  console.log(object)
+  title = object.song;
+  artist = object.artist;
+  year  = object.year;
+  console.log(`${title} by ${artist}, published in ${year}`)
+}
+
+showCollection(0)
